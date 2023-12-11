1aa7c4c2a65168ba09abce784ab4b513e1d372a3
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-11 15:33:59 +0200

    Add files via upload

---
+++ codez_carry/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+name "CodeZ_Carry"
+author "Obsidian_CodeZ"
+description 'Carry Script By Obsidian_CodeZ'
+lua54 "yes"
+
+client_scripts {
+   "client.lua",
+}
+
+server_scripts {
+    "server.lua",
+}
