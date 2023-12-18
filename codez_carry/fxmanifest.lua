a704c8ca4d7eddf3454221a46208872492372ce2
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-19 01:47:29 +0200

    Update fxmanifest.lua

--- codez_carry/fxmanifest.lua
+++ codez_carry/fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 name "CodeZ_Carry"
 author "Obsidian_CodeZ"
 description 'Carry Script By Obsidian_CodeZ'
+contact 'https://discord.com/invite/8nQcNAYTju'
 lua54 "yes"
 
 client_scripts {
